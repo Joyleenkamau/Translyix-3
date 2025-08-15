b2f0fd174d464f49117f0b239a92b6f491711373
Joyleenkamau <96373308+Joyleenkamau@users.noreply.github.com>
2025-08-15 21:26:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true }, // LAN testing
+  server: { host: true },
+  build: {
+    rollupOptions: {
+      input: '/index.html', // make sure Vite resolves paths correctly
+    },
+  },
 })
